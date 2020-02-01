4670ac2f9b337db27f774fa369794db44ce017e6
Kamilla Ruzhanska <kruzhanska@e2r14p9.unit.ua>
2020-02-01 16:23:02 +0200

    parser

--- src/mx_parsing.c
+++ src/mx_parsing.c
@@ -1,42 +1,60 @@
 #include "uls.h"
 
+static void add_to_Parser(char *argv, t_list **Parser, int k);
+static void print_Parser(t_list **Parser);
+static void distributor(char *argv, t_list **Parser);
+
 t_list **mx_parsing(int i, int argc, char **argv) {
     t_list **Parser = malloc(5 * sizeof(t_list *));
+    
+    for (int h = 0; h < 5; h++)
+        Parser[h] = NULL;
+    if (i == 0)
+        Parser[4] = mx_create_node(".");
+    else {
+        for (; i < argc; i++) {
+            distributor(argv[i], Parser);
+        }
+    }
+    print_Parser(Parser);
+    return Parser;
+}
+
+static void add_to_Parser(char *argv, t_list **Parser, int k) {
+    if (Parser[k] == NULL)
+        Parser[k] = mx_create_node(argv);
+    else
+        mx_push_back(&Parser[k], argv);
+}
+
+static void distributor(char *argv, t_list **Parser) {
     DIR *dir = NULL;
     int fd = 0;
 
-    for (int h = 0; h < 5; h++)
-        Parser[h] = NULL;
-    for (; i < argc; i++) {
-        fd = open(argv[i], O_RDONLY);
-        dir = opendir(argv[i]);
-        if (!dir) {
-            if (errno == ENOENT) {
-                if (Parser[1] == NULL)
-                    Parser[1] = mx_create_node(argv[i]);
-                else
-                    mx_push_back(&Parser[1], argv[i]);
-            }
-            if (fd > 0) {
-                if (Parser[2] == NULL)
-                    Parser[2] = mx_create_node(argv[i]);
-                else
-                    mx_push_back(&Parser[2], argv[i]);
-            }
-            if (errno == EACCES) {
-                if (Parser[3] == NULL)
-                    Parser[3] = mx_create_node(argv[i]);
-                else
-                    mx_push_back(&Parser[3], argv[i]);
-            }
-        }
-        if (dir) {
-            if (Parser[4] == NULL)
-                Parser[4] = mx_create_node(argv[i]);
-            else
-                mx_push_back(&Parser[4], argv[i]);
-        }
-        //closedir(dir);
+    fd = open(argv, O_RDONLY);
+    dir = opendir(argv);
+    if (!dir) {
+        if (errno == ENOENT)
+            add_to_Parser(argv, Parser, 1);
+        if (fd > 0)
+            add_to_Parser(argv, Parser, 2);
+        if (errno == EACCES)
+            add_to_Parser(argv, Parser, 3);
     }
-    return Parser;
+    else
+        add_to_Parser(argv, Parser, 4);
+}
+
+static void print_Parser(t_list **Parser) {
+    for (int i = 0; i < 5; i++) {
+        mx_printstr("Potok ");
+        mx_printint(i);
+        mx_printchar('\n');
+        while (Parser[i] != NULL) {
+            mx_printstr(Parser[i]->data);
+            mx_printchar(' ');
+            Parser[i] = Parser[i]->next;
+        }
+        mx_printchar('\n');
+    }
 }
--- src/mx_usage_error.c
+++ src/mx_usage_error.c
@@ -3,24 +3,24 @@
 static bool search_flag(char *str, char flag);
 
 t_list **mx_usage_error(int argc, char **argv) {
-    int i = 1; 
-    char *lsFlags = mx_strnew(3);
+    int i = 1;
+    int x = 0;
     //t_list trueFlags = NULL;
 
-    lsFlags = "laf";
-    for (; argv[i][0] == '-'; i++) {
-        for (int k = 1; argv[i][k] != '\0'; k++) {
-            if (argv[i][0] == '-') {
-                if (search_flag(lsFlags, argv[i][k]) == 0) {
-                    write(2, "uls: illegal option -- ", 23);
-                    write(2, &argv[i][k], 1);
-                    write(2, "\nusage: uls [-l] [file ...]\n", 28);
-                    exit(0);
-                }
-            }
+    for (; i < argc && x == 0; i++)
+        if (argv[i][0] == '-' && argv[i][1] != '\0') {
+            for (int k = 1; argv[i][k] != '\0'; k++)
+                if (argv[i][0] == '-') 
+                    if (search_flag("laf", argv[i][k]) == 0) {
+                        write(2, "uls: illegal option -- ", 23);
+                        write(2, &argv[i][k], 1);
+                        write(2, "\nusage: uls [-laf] [file ...]\n", 30);
+                        exit(0);
+                    }
         }
-    }
-    return mx_parsing(i, argc, argv);
+        else 
+            x = i;
+    return mx_parsing(x, argc, argv);
 }
 
 static bool search_flag(char *str, char flag) {
